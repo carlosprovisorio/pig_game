5c85ac7fd78003946f00e71ec790aa1b7b5079cf
Carlos <carlosprovisorio@icloud.com>
2017-08-07 16:31:58 -0400

    Implementing hold function and dry the code

--- app.js
+++ app.js
@@ -37,6 +37,31 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
+    nextPlayer();
+  }
+
+});
+
+  document.querySelector('.btn-hold').addEventListener('click',function() {
+    // Add Current score to global score
+      scores[activePlayer] += roundScore;
+    // Update the UI 
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    // Check if player won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+     // document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+      nextPlayer();
+    }
+  });
+
+
+
+  function nextPlayer() {
+
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
@@ -48,11 +73,6 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
     document.querySelector('.dice').style.display = 'none';
   }
-
-})
-
-
-
 
 
 
