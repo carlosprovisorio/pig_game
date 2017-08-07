336e3fdc4a3dffa30d6c430bd0edfedd482668e2
Carlos <carlosprovisorio@icloud.com>
2017-08-07 13:05:18 -0400

    Add events, Event handling: rolling the dice

--- app.js
+++ app.js
@@ -16,17 +16,36 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 + 1);
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</dice>';
-
-
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  //1. Ramdom number
+  var dice = Math.floor(Math.random() * 6 + 1);
+
+  //2. Display the result
+  var diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = 'dice-' + dice + '.png';
+  
+  //3. Update the round scorfe IF the rolled number was NOT a 1
+  
+
+})
 
 
+
+
+
+
+//var x = document.querySelector('#score-0').textContent;
+
+//dice = Math.floor(Math.random() * 6 + 1);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</dice>';
 
